4740c8aac1024178cfa69d45567b8e245bee53d1
viveksharma180 <sharma.viv@northeastern.edu>
2021-12-11 16:57:32 -0500

    Knowbourhood: added init image in police dashboard.
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- src/ui/screens/CreatePolice.java
+++ src/ui/screens/CreatePolice.java
@@ -16,6 +16,7 @@ public class CreatePolice extends javax.swing.JFrame {
      */
     public CreatePolice() {
         initComponents();
+        initImage();
     }
     /**
      * This method is called from within the constructor to initialize the form.
@@ -165,7 +166,42 @@ public class CreatePolice extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    
+        private void initImage() {
+        ImagePanel jPanel1 = new ImagePanel("src/assets/createpolice.jpg");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        
+        contactInput.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (contactInput.getText().length() >= 10) // limit textfield to 4 characters
+                {
+                    e.consume();
+                }
+            }
+        });
+    }
     
     /**
      * @param args the command line arguments
