78a771dfb3fc7fdc0b6cc178c95302d494e0036f
Chouhan Rohit <chouhan.ro@northeastern.edu>
2021-12-11 17:04:54 -0500

    Knowbourhood: Create route for AdminPostbox button
    Signed-off-by: Chouhan Rohit <chouhan.ro@northeastern.edu>

--- src/ui/screens/AdminDashboard.java
+++ src/ui/screens/AdminDashboard.java
@@ -185,7 +185,9 @@ public class AdminDashboard extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    //        
+        super.dispose();
+        AdminPostbox rd = new AdminPostbox();
+        rd.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
