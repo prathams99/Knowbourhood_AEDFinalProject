47c7d697179f7662472ba6d5ccafd5271587a1fb
viveksharma180 <sharma.viv@northeastern.edu>
2021-12-11 01:42:02 -0500

    Knowbourhood: Adding singleton socket in utils.[WIP]
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

---
+++ src/utils/MySingletonSocket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.io.IOException;
+import java.net.Socket;
+/**
+ *
+ * @author viveksharma
+ */
+public class MySingletonSocket {
+    
+    private static Socket clientSocket;
+}
