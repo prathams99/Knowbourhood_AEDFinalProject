706373ceff99438a5a247e237da2de1c25c6dd4c
prathams99 <sahasrabuddhe.p@northeastern.edu>
2021-12-11 01:56:51 -0500

    Knowbourhood: Fix merge conflicts
    - Get rid of head and tails
    
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

--- src/utils/ImagePanel.java
+++ src/utils/ImagePanel.java
@@ -5,10 +5,8 @@
  */
 package utils;
 
-<<<<<<< HEAD
-=======
+
 import java.awt.Graphics;
->>>>>>> 7a011ce1dc81068f26fcf59a729edc7cf780f62e
 import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -25,17 +23,15 @@ public class ImagePanel extends JPanel {
         backImage = new ImageIcon(imageName);
     }
 
-<<<<<<< HEAD
-    private BufferedImage getScaledImage() {
-        BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-
-        return image;
-=======
     @Override
     protected void paintComponent(Graphics g) {
         BufferedImage scaledImage = getScaledImage();
         super.paintComponent(g);
         g.drawImage(scaledImage, 0, 0, null);
->>>>>>> 7a011ce1dc81068f26fcf59a729edc7cf780f62e
+    }
+
+    private BufferedImage getScaledImage() {
+        BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        return image;
     }
 }
