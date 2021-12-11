d9795db17f769de74bfef5c655f8c729e1f006a8
prathams99 <sahasrabuddhe.p@northeastern.edu>
2021-12-11 01:17:26 -0500

    Knowbourhood: Create a class for showing GIF
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

---
+++ src/utils/Saving.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import javax.swing.JWindow;
+
+/**
+ *
+ * @author prath
+ */
+public class Saving extends JWindow {
+
+    public static boolean isDone = false;
+    
+    public static void main(String[] args) {
+        new LoadingScreen();
+    }    
+}
