e4203c53aa4e893aed83496e7bfb2ee420a436a8
viveksharma180 <sharma.viv@northeastern.edu>
2021-12-11 17:22:53 -0500

    Knowbourhood: Completed createPolice class along with background images.
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- src/ui/screens/PoliceManagement.java
+++ src/ui/screens/PoliceManagement.java
@@ -5,6 +5,7 @@
  */
 package ui.screens;
 
+import ui.login.LoginPanel;
 import utils.ImagePanel;
 
 /**
@@ -135,15 +136,21 @@ public class PoliceManagement extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
+        super.dispose();
+        CreatePolice rd = new CreatePolice();
+        rd.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-
+super.dispose();
+        UpdatePolice cd = new UpdatePolice();
+        cd.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-
+super.dispose();
+        ViewPolice vu = new ViewPolice();
+        vu.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
