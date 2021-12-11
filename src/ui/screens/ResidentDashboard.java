60cdb67e9694a8bc86d53efdd90e4934e0aa7812
prathams99 <sahasrabuddhe.p@northeastern.edu>
2021-12-11 17:58:26 -0500

    Knowbourhood: Add button for skipping reports
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

--- src/ui/screens/ResidentDashboard.java
+++ src/ui/screens/ResidentDashboard.java
@@ -400,7 +400,17 @@ public class ResidentDashboard extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-
+        if (currentEncounter > 0) {
+            currentEncounter--;
+            News encounter = userNews.get(currentEncounter);
+            subjectTitle.setText(encounter.getSubject());
+            messageTitle.setText(encounter.getMessage());
+            mailTitle.setText(encounter.getEmail());
+            dateTitle.setText(encounter.getDate() + "");
+            encounterNumber.setText(currentEncounter+1 + " / " + String.valueOf(totalEncounters));
+        } else {
+            JOptionPane.showMessageDialog(this, "There are only " + String.valueOf(totalEncounters) + " messages for " + r.getName());
+        }
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void messageTitleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_messageTitleActionPerformed
