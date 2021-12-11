37d3c7f2f51ab0464a018582eb9436cb07a614a0
Chouhan Rohit <chouhan.ro@northeastern.edu>
2021-12-11 00:56:07 -0500

    Knowbourhood: Add Date Validation in FileUtils
    Signed-off-by: Chouhan Rohit <chouhan.ro@northeastern.edu>

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,3 +1,7 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,6 +16,7 @@ public class FileUtils {
     private static final String nameRegex = "^[a-zA-Z ]+$";
      private static final String numberRegex = "^[0-9.]*$";
      private static final String nameNumberRegex = "^[a-zA-Z0-9-, ]+$";
+     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("MM-DD-YYYY");
     
      public static boolean validateName(String inputName) {
         return inputName.matches(nameRegex);
@@ -22,6 +27,15 @@ public class FileUtils {
     }
      public static boolean validateNameNumber(String nameNumberInput) {
         return nameNumberInput.matches(nameNumberRegex);
+    }
+     
+      public static boolean isValidDate(String input) {
+        try {
+            dateFormat.parse(input);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
     }
     
 }
