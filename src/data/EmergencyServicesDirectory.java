196fb240619e7296201f92a2f86bb22299e81e40
Chouhan Rohit <chouhan.ro@northeastern.edu>
2021-12-11 00:36:33 -0500

    Knowbourhood: Add Emergency Services Directory Java template
    Signed-off-by: Chouhan Rohit <chouhan.ro@northeastern.edu>

--- src/data/EmergencyServicesDirectory.java
+++ src/data/EmergencyServicesDirectory.java
@@ -4,10 +4,75 @@
  */
 package data;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author jarvis
  */
 public class EmergencyServicesDirectory {
+      private static EmergencyServicesDirectory personDirectory;
+    public static ArrayList<EmergencyServices> person;
+
+    public EmergencyServicesDirectory() {
+        person = new ArrayList<>();
+        ArrayList<Police> re = FileUtils.readPolice();
+        ArrayList<Fireman> ca = FileUtils.readFiremen();
+        ArrayList<Doctor> dr = FileUtils.readDoctors();
+        EmergencyServices person1 = new EmergencyServices(re, ca, dr);     
+        person.add(person1);
+        FileUtils.writeEs(person);
+        Gson g = new Gson();
+        String json = FileUtils.readFile("src/database/es-enterprise.json");
+        person = g.fromJson(json, new TypeToken<List<EmergencyServices>>() {
+        }.getType());
+        System.out.println("HUEHEUE" + "LALALLA" + person.toString());    
+    }
+
+    private ArrayList<Resident> getResident() {
+        ArrayList<Resident> resident = FileUtils.readResidents();
+        for (int i = 0; i < resident.size(); i++) {
+            return resident;
+        }
+        return null;
+    }
+
+    private ArrayList<CommunityAdmin> getCAdmin() {
+        ArrayList<CommunityAdmin> ca = FileUtils.readCommunityAdmins();
+        for (int i = 0; i < ca.size(); i++) {
+            return ca;
+        }
+        return null;
+    }
+
+    private Date convert(String stringDate) {
+        try {
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = formatter.parse(stringDate);
+            return date;
+        } catch (ParseException ex) {
+            Logger.getLogger(UsersDirectory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public List<EmergencyServices> getPersonList() {
+        return person;
+    }
+
+    public List<EmergencyServices> getPatientList() {
+        ArrayList<EmergencyServices> newPatient = new ArrayList<EmergencyServices>();
+        for (EmergencyServices p : person) {
+        }
+        return newPatient;
+    }
+    
     
 }
