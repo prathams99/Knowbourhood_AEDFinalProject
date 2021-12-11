b0f141e6c826d082830155317fd613a9302adc7e
Chouhan Rohit <chouhan.ro@northeastern.edu>
2021-12-11 17:08:37 -0500

    Knowbourhood: Created Action Performed for UserManagement button
    Signed-off-by: Chouhan Rohit <chouhan.ro@northeastern.edu>

--- src/ui/screens/AdminDashboard.java
+++ src/ui/screens/AdminDashboard.java
@@ -197,7 +197,9 @@ public class AdminDashboard extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-    //
+        super.dispose();
+        UserManagement vu = new UserManagement();
+        vu.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
