03e2f5db130614880e62d23a8a0caaa0c3555497
viveksharma180 <sharma.viv@northeastern.edu>
2021-12-11 18:51:46 -0500

    Knowbourhood: Volunteer Dashboard.
    -Added initImage Class.
    
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- src/ui/screens/VolunteerDashboard.java
+++ src/ui/screens/VolunteerDashboard.java
@@ -5,6 +5,8 @@
  */
 package ui.screens;
 
+import utils.ImagePanel;
+
 /**
  *
  * @author viveksharma
@@ -16,6 +18,8 @@ public class VolunteerDashboard extends javax.swing.JFrame {
      */
     public VolunteerDashboard() {
         initComponents();
+        initImage();
+
     }
 
     /**
@@ -254,6 +258,35 @@ public class VolunteerDashboard extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 //        
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void initImage() {
+        //residentName.setText(r.getName() + " may not necessarily have time, but definitely has a big heart.");
+        ImagePanel jPanel1 = new ImagePanel("src/assets/volunteer.jpg");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }
     
     /**
      * @param args the command line arguments
