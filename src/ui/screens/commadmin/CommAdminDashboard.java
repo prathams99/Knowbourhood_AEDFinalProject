c2321ffadf26118377edd201136e68c2f1cde42e
prathams99 <sahasrabuddhe.p@northeastern.edu>
2021-12-11 19:48:58 -0500

    Knowbourhood: Move CommunityAdmin screens into commadmin package
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

--- src/ui/screens/commadmin/CommAdminDashboard.java
+++ src/ui/screens/commadmin/CommAdminDashboard.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ui.screens;
+package ui.screens.commadmin;
 
 import data.CommunityAdmin;
 import ui.login.LoginPanel;
